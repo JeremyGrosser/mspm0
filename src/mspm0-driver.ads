0202c58054db08a7d2f3e06344fe3c2696cb3706
Jeremy Grosser <jeremy@synack.me>
2024-07-13 19:27:25 -0700

    dead code removal

--- src/mspm0-driver.ads
+++
@@ -1,9 +0,0 @@
---
---  Copyright (C) 2024 Jeremy Grosser <jeremy@synack.me>
---
---  SPDX-License-Identifier: BSD-3-Clause
---
-package MSPM0.Driver
-   with Pure
-is
-end MSPM0.Driver;
--- src/mspm0-spi.ads
+++
@@ -1,8 +0,0 @@
---
---  Copyright (C) 2024 Jeremy Grosser <jeremy@synack.me>
---
---  SPDX-License-Identifier: BSD-3-Clause
---
-package MSPM0.SPI is
-
-end MSPM0.SPI;
