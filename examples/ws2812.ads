52e7e2fbbf597e6a6283a3a6d30d930a052a7bb4
Jeremy Grosser <jeremy@synack.me>
2025-03-12 22:00:26 -0700

    more comments

--- examples/ws2812.ads
+++ examples/ws2812.ads
@@ -3,6 +3,14 @@
 --
 --  SPDX-License-Identifier: BSD-3-Clause
 --
+--  Driver for a chain of WS2812B RGB LED modules. Uses a TIMG module and DMA.
+--  TIMG CCVAL sets the duration of a bit pulse.
+--  On TIMG LOAD, event group 0 triggers DMA to write the next CCVAL.
+--  DMA is triggered once for each of 24 bits per LED.
+--  After LED_Count * 24 bits are transferred, the DMA transfer is complete and triggers a CPU interrupt.
+--  The CPU interrupt stops the TIMG counter and sets the counter value to 0,
+--   forcing the output low during idle periods.
+--
 with MSPM0; use MSPM0;
 
 package WS2812 is
