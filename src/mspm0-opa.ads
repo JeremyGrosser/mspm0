dc929eb87cc8f9cd4eeededd1fbbf28ae26aba21
Jeremy Grosser <jeremy@synack.me>
2026-04-21 11:04:41 -0700

    CFGBASE

--- src/mspm0-opa.ads
+++ src/mspm0-opa.ads
@@ -2,6 +2,19 @@ package MSPM0.OPA
    with Pure
 is
 
+   type CFGBASE_Register is record
+      RRI : Boolean := False;
+      GBW : Boolean := False;
+   end record
+      with Volatile_Full_Access,
+           Effective_Writes,
+           Async_Readers,
+           Object_Size => 32;
+   for CFGBASE_Register use record
+      RRI at 0 range 2 .. 2;
+      GBW at 0 range 0 .. 0;
+   end record;
+
    type CFG_Register is record
       GAIN     : UInt3 := 0;
       MSEL     : UInt3 := 0;
@@ -27,6 +40,7 @@ is
       PWREN    : UInt32;
       RSTCTL   : UInt32;
       CTL      : UInt32;
+      CFGBASE  : CFGBASE_Register;
       CFG      : CFG_Register;
       STAT     : UInt32;
    end record
@@ -36,6 +50,7 @@ is
       PWREN    at 16#0800# range 0 .. 31;
       RSTCTL   at 16#0804# range 0 .. 31;
       CTL      at 16#1100# range 0 .. 31;
+      CFGBASE  at 16#1104# range 0 .. 31;
       CFG      at 16#1108# range 0 .. 31;
       STAT     at 16#1118# range 0 .. 31;
    end record;
